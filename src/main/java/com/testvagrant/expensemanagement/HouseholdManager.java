bda762235464a9a1fee51c4b29d2e45d34d7ab41
Phani Ranjan K V S <phaniranjan@gmail.com>
2020-03-20 21:47:19 +0530

    Adding few properties and minimal logging statements

--- src/main/java/com/testvagrant/expensemanagement/HouseholdManager.java
+++ src/main/java/com/testvagrant/expensemanagement/HouseholdManager.java
@@ -6,25 +6,31 @@ import java.util.Calendar;
 public class HouseholdManager {
 
 	public static void main(String[] args) {
-		NewsPaperExpense paperSubscription1 = new NewsPaperExpense("TOI","ET","BM");
-		NewsPaperExpense paperSubscription2 = new NewsPaperExpense("TOI","Hindu");
-		NewsPaperExpense paperSubscription3 = new NewsPaperExpense("TOI","ET","BM","Hindu","HT");
-		
-		HouseholdExpenses household1 = new HouseholdExpenses();
-		HouseholdExpenses household2 = new HouseholdExpenses();
-		HouseholdExpenses household3 = new HouseholdExpenses();
-		
+		NewsPaperExpense paperSubscription1 = new NewsPaperExpense("TOI", "ET", "BM");
+		NewsPaperExpense paperSubscription2 = new NewsPaperExpense("TOI", "Hindu");
+		NewsPaperExpense paperSubscription3 = new NewsPaperExpense("TOI", "ET", "BM", "Hindu", "HT");
+
+		HouseholdExpenses household1 = new HouseholdExpenses("Jishnu", "Bengaluru", "Marathahalli");
+		HouseholdExpenses household2 = new HouseholdExpenses("Jignesh", "Hyderabad", "Gandipet");
+		HouseholdExpenses household3 = new HouseholdExpenses("Kishore", "Kurnool", "BirlaGate");
+
 		System.out.println("Household Expenses for current month :"
 				+ new SimpleDateFormat("MMMM").format(Calendar.getInstance().getTime()));
-		
-		household1.addExpense(paperSubscription1);
-		System.out.println("Total monthly Bill for house hold1: " + household1.calculateExpense());
 
-		household2.addExpense(paperSubscription2);
-		System.out.println("Total monthly Bill for house hold2:  " + household2.calculateExpense());
+		printHouseHoldBillingDetails(paperSubscription1, household1);
 
-		household3.addExpense(paperSubscription3);
-		System.out.println("Total monthly Bill for house hold3:  " + household3.calculateExpense());
+		printHouseHoldBillingDetails(paperSubscription2, household2);
+
+		printHouseHoldBillingDetails(paperSubscription3, household3);
+
+	}
+
+	private static void printHouseHoldBillingDetails(NewsPaperExpense paperSubscription, HouseholdExpenses household) {
+
+		System.out.println("Monthly household expesne details for " + household.getOwnerName());
+		household.addExpense(paperSubscription);
+		System.out.println("Total monthly bill :" + household.calculateExpense());
+		System.out.println("");
 	}
 
 }
