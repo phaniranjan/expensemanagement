03550001ad40ee0cf797ff4e9c15558790ed0a3c
Phani Ranjan K V S <phaniranjan@gmail.com>
2020-03-20 19:22:08 +0530

    Refactoring news paper data in to separate class and modifying print statements

--- src/main/java/com/testvagrant/expensemanagement/NewsPaperExpense.java
+++ src/main/java/com/testvagrant/expensemanagement/NewsPaperExpense.java
@@ -1,72 +1,29 @@
 
 package com.testvagrant.expensemanagement;
 
+import com.testvagrant.expensemanagement.data.NewsPaperData;
+
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.HashMap;
 
 public class NewsPaperExpense implements Expense {
 
-	final double newsPaperCost[][] = { { 3.0, 3.0, 3.0, 3.0, 3.0, 5.0, 6.0 }, { 2.5, 2.5, 2.5, 2.5, 2.5, 4.0, 4.0 },
-			{ 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 10 }, { 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5 },
-			{ 2.0, 2.0, 2.0, 2.0, 2.0, 4.0, 4.0 } };
-	private HashMap<String, Integer> paperTypePriceList = new HashMap<String, Integer>() {
-		{
-			put("TOI", 0);
-			put("Hindu", 1);
-			put("ET", 2);
-			put("BM", 3);
-			put("HT", 4);
-		}
-
-	};
-
 	private double monthlyExpense = 0.0;
 	private ArrayList<String> optedNewsPapers;
+	private NewsPaperData newsPaperData;
 
 	public NewsPaperExpense(String... subscribedNewsPapers) {
 		optedNewsPapers = new ArrayList<String>();
 		Collections.addAll(optedNewsPapers, subscribedNewsPapers);
+		newsPaperData = new NewsPaperData();
 
-	}
-
-	double calculateCurrentMonthlyExpense(String paperType) {
-		Calendar calendar = Calendar.getInstance();
-		int month = calendar.get(Calendar.MONTH);
-		int monthDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-		System.out.println("Month " + month + " monthDays " + monthDays);
-		calendar.set(Calendar.DAY_OF_MONTH, 1);
-		int firstDayOfMonth = calendar.get(Calendar.DAY_OF_WEEK);
-		System.out.println("First day of Month is: " + firstDayOfMonth);
-		System.out.println(calendar.getTime());
-
-		HashMap<Integer, Integer> dayOfWeekCountMap = new HashMap<Integer, Integer>();
-
-		int balanceDays = monthDays % 7;
-
-		for (int i = 0; i < 7; i++) {
-			dayOfWeekCountMap.put(firstDayOfMonth + i > 7 ? (firstDayOfMonth + i - 7) : firstDayOfMonth + i,
-					(balanceDays) > 0 ? 5 : 4);
-			balanceDays--;
-		}
-
-		System.out.println(dayOfWeekCountMap.toString());
-
-		double monthTotalBillNewsPaper = 0.0;
-
-		for (int i = 1; i <= 7; i++) {
-			monthTotalBillNewsPaper += dayOfWeekCountMap.get(i)
-					* newsPaperCost[paperTypePriceList.get(paperType)][i - 1];
-		}
-		System.out.println("Month Total bill for " + paperType + " :" + monthTotalBillNewsPaper);
-		return monthTotalBillNewsPaper;
 	}
 
 	public double getMonthlyExpesne() {
+		
 		for (String paperType : optedNewsPapers) {
-			monthlyExpense += calculateCurrentMonthlyExpense(paperType);
+
+			monthlyExpense += newsPaperData.calculateCurrentMonthlyExpense(paperType);
 		}
 
 		return monthlyExpense;
---
+++ src/main/java/com/testvagrant/expensemanagement/data/NewsPaperData.java
@@ -0,0 +1,58 @@
+package com.testvagrant.expensemanagement.data;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+public final class NewsPaperData {
+	
+	final double newsPaperCost[][] = { { 3.0, 3.0, 3.0, 3.0, 3.0, 5.0, 6.0 }, { 2.5, 2.5, 2.5, 2.5, 2.5, 4.0, 4.0 },
+			{ 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 10 }, { 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5 },
+			{ 2.0, 2.0, 2.0, 2.0, 2.0, 4.0, 4.0 } };
+	
+	private HashMap<String, Integer> paperTypePriceList;
+	private Calendar calendar = Calendar.getInstance();
+	private HashMap<Integer, Integer> dayOfWeekCountMap;
+	
+	public NewsPaperData() {
+		paperTypePriceList = new HashMap<String, Integer>() {
+			{
+				put("TOI", 0);
+				put("Hindu", 1);
+				put("ET", 2);
+				put("BM", 3);
+				put("HT", 4);
+			}
+		};
+		populateDaysOfCurrentMonthCount();
+	}
+	
+	public double calculateCurrentMonthlyExpense(String paperType) {
+		
+		double monthTotalBillNewsPaper = 0.0;
+
+		for (int i = 1; i <= 7; i++) {
+			monthTotalBillNewsPaper += this.dayOfWeekCountMap.get(i)
+					* newsPaperCost[paperTypePriceList.get(paperType)][i - 1];
+		}
+		System.out.println("Monthly bill for " + paperType + " :" + monthTotalBillNewsPaper);
+		return monthTotalBillNewsPaper;
+	}
+
+	private void populateDaysOfCurrentMonthCount(){
+		int monthDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		int firstDayOfMonth = calendar.get(Calendar.DAY_OF_WEEK);
+		
+		this.dayOfWeekCountMap = new HashMap<Integer, Integer>();
+ 
+		int balanceDays = monthDays % 7;
+
+		for (int i = 0; i < 7; i++) {
+			this.dayOfWeekCountMap.put(firstDayOfMonth + i > 7 ? (firstDayOfMonth + i - 7) : firstDayOfMonth + i,
+					(balanceDays) > 0 ? 5 : 4);
+			balanceDays--;
+		}
+		
+	}
+
+}
